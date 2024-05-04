12749c42e6d78115906db8424e6fb8233b7e8276
zhang jing yi <zhangjingyi@zhangs-MacBook-Pro.local>
2024-05-03 22:48:54 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Hello, World!</h1>
+        <div>
+          <h2>JINGYI ZHANG</h2>
+          <p>jyz0328@bu.edu</p>
+        </div>
+        <div>
+          <h2>Major:Electrical and Computer Engineer</h2>
+          <p>minor: data analysis</p>
+        </div>
+        <div>
+          <h2>Summer main plan: do an internship</h2>
+          <p>summer minous plan: travel in USA</p>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
+
+
